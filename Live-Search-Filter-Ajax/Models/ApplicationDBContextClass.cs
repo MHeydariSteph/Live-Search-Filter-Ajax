816452d716b2705bf4f566424252c6d0ccd007eb
Mark Heydari <MHeydari@stephensons.ca>
2020-09-25 09:16:20 -0400

    Add project files.

---
+++ Live-Search-Filter-Ajax/Models/ApplicationDBContextClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Live_Search_Filter_Ajax.Modals;
+
+
+namespace Live_Search_Filter_Ajax.Modals
+{
+    public class ApplicationDBContnextClass : DbContext
+    {
+        public ApplicationDBContnextClass(DbContextOptions<ApplicationDBContnextClass> options):base(options)
+        {
+
+
+        }
+        public DbSet<DispClass> Dispute { get; set; }
+    }
+}
---
+++ Live-Search-Filter-Ajax/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Live_Search_Filter_Ajax.Modals;
+
+namespace Live_Search_Filter_Ajax.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDBContnextClass _adb;
+
+        public IndexModel(ApplicationDBContnextClass adb)
+        {
+            _adb = adb;
+        }
+        public IEnumerable<DispClass> results { get; set; }
+
+        public void OnGet()
+        {
+            results = _adb.Dispute.ToList();
+        }
+    }
+}
+
